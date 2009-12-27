82319e60b216e9de77dc1815aa82b659123512ec
eugeny.grishul <eugeny.grishul@f413ba3e-df7c-11dd-987e-d7c0a4ebcf36>
2009-12-27 05:52:15 +0000

    add intel perfromance results

--- trunk/PerformanceTests/PerformanceVisualizer.cs
+++ trunk/PerformanceTests/PerformanceVisualizer.cs
@@ -24,30 +24,38 @@ namespace NObjective
 	{
 		public static void Main( string[] parameters )
 		{
-			var files = Directory.GetFiles( ".", "*.Results.xml" );
-			var libraryNames = files.Select( x => x.Remove( x.Length - 12 ) ).ToArray();
-			var results = files.Select( x => PerformanceTestResult.Load( x ) ).ToArray();
-
-			var categories = results.SelectMany( x => x.Entries ).Select( x => x.Name ).Distinct().ToArray();
-
-			using( var htmlWriter = new StreamWriter( "TestResults.htm" ) )
-			using( var wikiWriter = new StreamWriter( "TestResults.wiki" ) )
+			foreach( var platform in Directory.GetDirectories( "." ) )
 			{
-				htmlWriter.WriteLine( "<html><body>" );
-				foreach( var category in categories )
+				foreach( var os in Directory.GetDirectories( platform ) )
 				{
-					var entries = results.Select( x => new { Entry = x[category], Name = x.AssemblyName } ).OrderByDescending( x => x.Name ).ToArray();
+					var files = Directory.GetFiles( os, "*.Results.xml" );
+					if( files.Length == 0 ) continue;
 
-					var performanceValues = entries.Select( x => x.Entry == null ? 0 : ( double ) x.Entry.Score ).ToArray();
-					var maximumValue = performanceValues.Max();
-					var normalizedValues = performanceValues.Select( x => ( x * 100.0 / maximumValue ).ToString( CultureInfo.InvariantCulture ) ).ToArray();
+					var libraryNames = files.Select( x => x.Remove( x.Length - 12 ) ).ToArray();
+					var results = files.Select( x => PerformanceTestResult.Load( x ) ).ToArray();
 
-					var url = string.Format( @"http://chart.apis.google.com/chart?chtt={5}&chts=000000,12&chs=1000x120&chf=bg,s,ffffff|c,s,ffffff&chxt=x,y&chxl=1:||0:|0|{1}|{2}|{3}&cht=bhg&chd=t:{4}&chdl={0}&chco=cc0000,003399,ff00cc,cc6600&chbh=10&nonsense=something_that_ends_with.png", string.Join( "|", entries.Select( x => x.Name ).ToArray() ), ( long ) ( maximumValue / 3 ), ( long ) ( maximumValue * 2 / 3 ), maximumValue, string.Join( "|", normalizedValues ), Escape( category ) );
+					var categories = results.SelectMany( x => x.Entries ).Select( x => x.Name ).Distinct().ToArray();
 
-					htmlWriter.WriteLine( @"<img src=""{0}""/>", url );
-					wikiWriter.WriteLine( "{0}", url );
+					using( var htmlWriter = new StreamWriter( Path.Combine( os, "TestResults.htm" ) ) )
+					using( var wikiWriter = new StreamWriter( Path.Combine( os, "TestResults.wiki" ) ) )
+					{
+						htmlWriter.WriteLine( "<html><body>" );
+						foreach( var category in categories )
+						{
+							var entries = results.Select( x => new { Entry = x[category], Name = x.AssemblyName } ).OrderByDescending( x => x.Name ).ToArray();
+
+							var performanceValues = entries.Select( x => x.Entry == null ? 0 : ( double ) x.Entry.Score ).ToArray();
+							var maximumValue = performanceValues.Max();
+							var normalizedValues = performanceValues.Select( x => ( x * 100.0 / maximumValue ).ToString( CultureInfo.InvariantCulture ) ).ToArray();
+
+							var url = string.Format( @"http://chart.apis.google.com/chart?chtt={5}&chts=000000,12&chs=1000x120&chf=bg,s,ffffff|c,s,ffffff&chxt=x,y&chxl=1:||0:|0|{1}|{2}|{3}&cht=bhg&chd=t:{4}&chdl={0}&chco=cc0000,003399,ff00cc,cc6600&chbh=10&nonsense=something_that_ends_with.png", string.Join( "|", entries.Select( x => x.Name ).ToArray() ), ( long ) ( maximumValue / 3 ), ( long ) ( maximumValue * 2 / 3 ), maximumValue, string.Join( "|", normalizedValues ), Escape( category ) );
+
+							htmlWriter.WriteLine( @"<img src=""{0}""/>", url );
+							wikiWriter.WriteLine( "{0}", url );
+						}
+						htmlWriter.WriteLine( "</body></html>" );
+					}
 				}
-				htmlWriter.WriteLine( "</body></html>" );
 			}
 		}
 
